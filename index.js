c929a7e57b43e67cd22888342659e20b692ea041
Evans Njeru <evansnjerudev@gmail.com>
2022-08-27 16:59:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function filter(drivers, fn) {
+	const matches = [];
+	drivers.forEach(function (driver) {
+		if(fn(driver))
+			matches.push(driver);
+	});
+
+	return matches;
+}
+
+const findMatching = function (drivers, str) {
+	return filter(drivers, function (driver) {
+		return driver.toLowerCase() === str.toLowerCase();
+	});
+};
+
+
+const fuzzyMatch = function (drivers, str) {
+	return filter(drivers, function (driver) {
+		return driver.slice(0, str.length).toLowerCase() == str.toLowerCase();
+	});
+};
+
+
+const matchName = function (drivers, str) {
+	return filter(drivers, function (driver) {
+		return driver["name"].toLowerCase() == str.toLowerCase();
+	});
+};
